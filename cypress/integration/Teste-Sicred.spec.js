ce6a0b61b551875b0e342b65c170ddda5282b636
Karina Araujo <karinaaraujo.8891@gmail.com>
2022-09-13 18:08:18 -0300

    commit initial

---
+++ cypress/integration/Teste-Sicred.spec.js
@@ -0,0 +1,69 @@
+
+/// <reference types="cypress" />
+
+
+describe('Grocery crud',function() {
+    
+    beforeEach(function(){
+    cy.visit('https://www.grocerycrud.com/v1.x/demo/bootstrap_theme');
+    })
+    it.only('Verificar o titulo da aplicação', function() {
+        cy.title().should('be.equal', 'Grocery CRUD Demo - Bootstrap Theme');
+    })
+    
+    it.only('Selecionar versao', () => {
+        cy.get('#switch-version-select').select('/v1.x/demo/bootstrap_theme_v4');
+
+    it.only('clicar em addcustomer',) 
+        cy.get('.floatL.t5 > .btn').click();
+    
+    it.only('preencher nome',)
+        cy.get('#field-customerName').type('Teste Sicred');
+
+    it.only('preencher sobrenome',)
+    cy.get('input[id="field-contactLastName"]').type('Teste');
+
+    it.only('preencher contactFirstName', )
+    cy.get('#field-contactFirstName').type('Karina Araujo');
+
+    it.only('preecher telefone',)
+    cy.get('#field-phone').type('51 9999-9999');
+
+   it.only('preencher endereço linha1',)
+   cy.get('#field-addressLine1').type('Av Assis Brasil, 3970');
+
+   it.only('preencher endereço linha2',)
+   cy.get('#field-addressLine2').type('Torre D');
+
+   it.only('preencher cidade',)
+   cy.get('#field-city').type('Porto Alegre')
+   
+
+   it.only('preencher estado',)
+   cy.get('#field-state').type('RS')
+
+   it.only('codigo postal',)
+   cy.get('#field-postalCode').type('91000-000')
+
+   it.only('preecher país',)
+   cy.get('#field-country').type('Brasil')
+
+   it.only('selecionar funcionario',)
+   //cy.get('a.chosen-single').select('Fixter');
+   
+   cy.get('a.chosen-single').click() 
+   cy.xpath('//li[contains(text(),"Fixter")]').click()
+   
+   it.only('preencher limite de credito',)
+   cy.get('#field-creditLimit').type('200')
+
+   it.only('clicar em salvar',)
+   cy.get('#form-button-save').click();
+   
+  it.only('trazer mensagem de sucesso',)
+  cy.contains('#report-success', 'Your data has been successfully stored into the database. Edit Customer or Go back to list')
+ 
+  cy.exec('taskkill /F /IM cypress.exe')
+})
+
+})
